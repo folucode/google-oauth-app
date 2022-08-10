60e491897abf5d9af6a943bfb6db1154d31708ca
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2022-08-10 11:54:23 +0100

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { GoogleOAuthGuard } from './google-oauth.guard';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('auth')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @UseGuards(GoogleOAuthGuard)
+  async googleAuth(@Request() req) {}
+
+  @Get('google-redirect')
+  @UseGuards(GoogleOAuthGuard)
+  googleAuthRedirect(@Request() req) {
+    return this.appService.googleLogin(req);
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { GoogleStrategy } from './google.strategy';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService, GoogleStrategy],
+})
+export class AppModule {}
